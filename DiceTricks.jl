0b3465a3617e187f5f85388c9f008189168cde54
emily <epryer@student.fitchburgstate.edu>
2024-12-10 16:28:31 -0500

    Added Exploding Dice Game.

--- DiceTricks.jl
+++ DiceTricks.jl
@@ -95,4 +95,36 @@ module DiceTricks
         println("Standard Deviation: $(std(scores))")
     end
 
+    #Exploding Dice Game
+    """
+    explode(mad::Int)
+    Enter max sides on single die. Roll until result is not max, add all rolls together.
+    """
+    function explode(max::Int)
+        local sum=0
+        if max<0
+            throw(ArgumentError("This number on a n-sided die has to be greater than 0"))
+        end
+        check = max
+        roll = rand(1:max)
+        while roll == check
+            sum += roll 
+            roll = rand(1:max)
+        end
+        sum += roll
+        return sum
+    end
+
+    """
+    mutiple_explodetrials(f::Function, trials::Int, max::Int)
+    Run trials on explode dice function. Returns vector of all scores.
+    """
+    function multiple_explodetrials(f::Function, trials::Int, max::Int)
+        local results = Int[]
+        for i in 1:trials 
+            push!(results, f(max))
+        end
+        return results
+    end 
+
 end #module DiceTricks
